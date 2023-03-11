9c5ae06903eda28492b1a3bd091eff3c0d69398c
Bruna Santiago <brunasanthiago@outlook.com>
2023-03-11 10:12:24 -0300

    refatorando os nomes

--- machine-learning/choice.py
+++ machine-learning/choice.py
@@ -1,4 +1,5 @@
 from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
 
 porco1 = [0, 1, 0]
 porco2 = [0, 1, 1]
@@ -9,22 +10,33 @@ cachorro2 = [1, 0, 1]
 cachorro3 = [1, 1, 1]
 
 # 1 = porco, 0 = cachorro
-dados = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3]
-classes = [1, 1, 1, 0, 0, 0]
+treino_x = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3]
+treino_y = [1, 1, 1, 0, 0, 0]
 
 model = LinearSVC()
 
-model.fit(dados, classes)
+model.fit(treino_x, treino_y)
 
 animal_misterioso = [1, 1, 1]
 teste = model.predict([animal_misterioso])
+# resposta esperada: [0]
 print(teste)
 
 misterio1 = [1, 1, 1]
 misterio2 = [1, 1, 0]
 misterio3 = [0, 1, 1]
 
-teste_misterioso = [misterio1, misterio2, misterio3]
+testes_x = [misterio1, misterio2, misterio3]
 
-print(model.predict(teste_misterioso))
-# resposta esperada: [0, 1, 1]
+teste_x = model.predict(testes_x)
+treino_y = [0, 1, 1]
+
+taxa_de_acerto = accuracy_score(treino_y, teste_x)
+
+
+def formatacao_texto(texto):
+    text = 'Taxa de acerto do algoritmo: {:.2f}%'.format(texto * 100)
+    return text
+
+
+print(formatacao_texto(taxa_de_acerto))
